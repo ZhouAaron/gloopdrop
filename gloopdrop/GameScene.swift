d79a7d07e262338199e43b81902aaafd29182c1a
ZhouJAY <zj971021@163.com>
2022-03-20 13:13:03 +0800

    adding animation and movement with actions

--- gloopdrop/GameScene.swift
+++ gloopdrop/GameScene.swift
@@ -9,17 +9,47 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    let player = Player()
+    let playerSPeed: CGFloat = 1.5
     override func didMove(to view: SKView) {
         // Set up background
         let background = SKSpriteNode(imageNamed: "background_1")
         background.anchorPoint = CGPoint(x: 0, y: 0)
         background.position = CGPoint(x: 0, y: 0)
+        background.zPosition = Layer.background.rawValue
         addChild(background)
         
         // Set up foreground
         let foreground = SKSpriteNode(imageNamed: "foreground_1")
         foreground.anchorPoint = CGPoint(x: 0, y: 0)
         foreground.position = CGPoint(x: 0, y: 0)
+        foreground.zPosition = Layer.foreground.rawValue
         addChild(foreground)
+        
+        // Set up player
+        player.position = CGPoint(x: size.width/2, y: foreground.frame.maxY)
+        addChild(player)
+        player.setupConstraints(floor: foreground.frame.maxY)
+        player.walk()
+    }
+    // MARK: - TOUCH HANDLING
+    /* ####################################################################### */
+    /*                        TOUCH HANDLERS STARTS HERE                       */
+    /* ####################################################################### */
+    func touchDown(atPoint pos : CGPoint) {
+        
+        // Calculate the speed based on current position and tap location
+        // hypot() uses a bit of trigonometry to calculate the distance between the two points.
+        let distance = hypot(pos.x - player.position.x, pos.y - player.position.y)
+        let calculatedSpeed = TimeInterval(distance / playerSPeed) / 255
+//        print(" distance: \(distance) \n claculatedSpeed: \(calculatedSpeed)")
+        if pos.x < player.position.x {
+            player.moveToPosition(pos: pos, direction: "L", speed: calculatedSpeed)
+        } else {
+            player.moveToPosition(pos: pos, direction: "R", speed: calculatedSpeed)
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
 }
---
+++ gloopdrop/SpriteKitHelper.swift
@@ -0,0 +1,57 @@
+//
+//  SpriteKitHelper.swift
+//  gloopdrop
+//
+//  Created by Aaron on 2022/3/20.
+//
+
+import Foundation
+import SpriteKit
+
+
+// MARK: - SPRITEKIT HELPERS
+
+// Set up shared z-positions
+enum Layer: CGFloat {
+    case background
+    case foreground
+    case player
+}
+
+// MARK: - SPRITEKIT EXTENSIONS
+
+extension SKSpriteNode {
+    // Used to load texture arrays for animations
+    // an atlas name, a prefix, and the start and stop frame numbers for the animation. It then uses a for-in loop to build and return the array of textures.
+    func loadTextures(atlas: String, prefix: String, startAt: Int, stopsAt: Int) -> [SKTexture] {
+        var textureArray = [SKTexture]()
+        let textureAtlas = SKTextureAtlas(named: atlas)
+        for i in startAt...stopsAt {
+            let textureName = "\(prefix)\(i)"
+            let temp = textureAtlas.textureNamed(textureName)
+            textureArray.append(temp)
+        }
+        return textureArray
+    }
+    
+    // Start the animation using a name and a count (0 = repeat forever)
+    func startAnimation(textures: [SKTexture], speed: Double, name: String, count: Int, resize: Bool, restore: Bool) {
+        // Run animation only if animation key doesn't already exist
+        if (action(forKey: name) == nil) {
+            // The first action sets up the frame-by-frame animation using the supplied textures. The code then cycles through these textures based on the value of timePerFrame. The timePerFrame is the amount of time, in seconds, each texture is displayed. The other parameters, resize and restore, indicate how the action should handle the size and final texture for the node. When resize = true, the sprite’s size matches the image size. When restore = true, the original texture is restored when the action completes.
+            let animation = SKAction.animate(with: textures, timePerFrame: speed, resize: resize, restore: restore)
+            if count == 0 {
+                // Run animation until stopped
+                // how many times to repeat the action
+                let repeatAction = SKAction.repeatForever(animation)
+                run(repeatAction, withKey: name)
+            } else if count == 1 {
+                run(animation, withKey: name)
+            } else {
+                // determine how often to repeat the action: when a value of 0 is passed into the method, the action repeats forever, whereas if any other number is passed in, the action repeats the specified number of times.
+                let repeatAction = SKAction.repeat(animation, count: count)
+                run(repeatAction, withKey: name)
+            }
+        }
+    }
+}
