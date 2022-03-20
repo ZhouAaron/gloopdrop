627dfe1223a47b148747ca050450a68dd7034dde
ZhouJAY <zj971021@163.com>
2022-03-20 10:47:07 +0800

    Charpt 1

--- gloopdrop/GameViewController.swift
+++ gloopdrop/GameViewController.swift
@@ -13,44 +13,46 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        // create the view
+        // 使用强制类型转换运算符（as！）的强制形式将视图向下转换为 SKView。
+        if let view = self.view as! SKView? {
             
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+            // Create the scene
+//            let scene = GameScene(size: view.bounds.size)
+            let scene = GameScene(size: CGSize(width: 1336, height: 1024))
+            
+            // set the scale mode to scale to fill the view window
+            scene.scaleMode = .aspectFill
+            
+            // set the background color
+            scene.backgroundColor = UIColor(red: 105/255, green: 157/255, blue: 181/255, alpha: 1.0)
+            
+            // present the scene
+            view.presentScene(scene)
+            
+            // set the view options
+            view.ignoresSiblingOrder = false // control the rendering order of the nodes.
+            view.showsPhysics = false //  show or hide the physics bodies attached to your nodes.
+            view.showsFPS = true //  show or hide the frames per second (FPS) indicator.
+            view.showsNodeCount = true //  used to show or hide the number of nodes.
         }
+        
+       
     }
 
     override var shouldAutorotate: Bool {
         return true
     }
 
+//    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+//        if UIDevice.current.userInterfaceIdiom == .phone {
+//            return .allButUpsideDown
+//        } else {
+//            return .all
+//        }
+//    }
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscape
     }
 
     override var prefersStatusBarHidden: Bool {
